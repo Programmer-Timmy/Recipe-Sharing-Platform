6a7aae865914472833503b8920f4d8b55612da1e
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-12-13 17:39:34 +0100

    when no recipe found show others

--- private/views/pages/recipe.php
+++ private/views/pages/recipe.php
@@ -1,10 +1,5 @@
 <?php
 $recipe = Recipes::getRecipe($_GET['id']);
-
-if (!$recipe) {
-    echo "Recipe not found!";
-    exit();
-}
 $commented = false;
 $comments = comments::getCommentsByRecipeId($recipe->id);
 
@@ -41,6 +36,7 @@ if (isset($_GET['delete'])) {
     comments::deleteComment($_GET['delete']);
     header("Location: recipe?id=" . $_GET['id']);
 }
+if ($recipe) :
 ?>
 
 <div class="container mt-4">
@@ -222,5 +218,48 @@ if (isset($_GET['delete'])) {
             </div>
         </div>
     </div>
-
 </div>
+<?php else: ?>
+    <div class="container text-center mt-4">
+        <h1>Oeps, geen recept gevonden!</h1>
+        <p>Het lijkt erop dat het recept dat je zoekt niet in onze collectie is. Geen zorgen, ontdek andere heerlijke
+            recepten of voeg je eigen smakelijke creaties toe aan CookCook Connect!</p>
+        <a href="recipes" class="btn btn-primary mt-1 mb-4">Ga terug</a>
+
+
+        <!-- Display three random recipes as alternatives -->
+        <div class="row mt-4" style="border: none; background-color: rgba(182,182,182,0.16); border-radius: 5px;">
+            <div class="col-md-12 mt-3">
+                <h2>Ontdek andere heerlijke recepten</h2>
+            </div>
+            <?php
+            $randomRecipes = Recipes::getRandomRecipes(); // Adjust the method based on your implementation
+
+            foreach ($randomRecipes as $recipe) {
+                echo "
+                <div class=\"col-md-4 mt-4 mb-4 d-flex\">
+                    <div class=\"card flex-fill\">
+                        <a href=\"recipe?id=$recipe->id\" class=\"h-100 d-flex flex-column card-link\">
+                            <img src=\"$recipe->img_url\" class=\"card-img-top\" alt=\"$recipe->title\">
+                            <div class=\"card-body\">
+                                <h5 class=\"card-title\">$recipe->title</h5>
+                                <p class=\"card-text\">$recipe->description</p>
+                            </div>
+                            <div class=\"card-footer d-flex justify-content-between flex-wrap\">";
+                $recipeCategories = Categories::getCategoriesByRecipes($recipe->id);
+                foreach ($recipeCategories as $category) {
+                    echo "<span class=\"badge bg-primary m-1\">" . $category->name . "</span>";
+                }
+                echo "
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            ";
+            }
+            ?>
+        </div>
+    </div>
+
+
+<?php endif; ?>
