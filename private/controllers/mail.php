95444a75e4e0963338b1e53f02df30eda44ec3ee
Programmer-Timmy <122127801+Programmer-Timmy@users.noreply.github.com>
2023-12-13 23:24:08 +0100

    email text added

--- private/controllers/mail.php
+++ private/controllers/mail.php
@@ -10,7 +10,7 @@ require '../static/phpmailer/src/SMTP.php';
 
 class mail
 {
-    public static function sendEmailVerification()
+    public static function sendEmailVerification($email, $firstname, $lastname, $token)
     {
         $mail = new PHPMailer(true);
 
@@ -22,18 +22,18 @@ class mail
             $mail->SMTPAuth = true;                                   //Enable SMTP authentication
             $mail->Username = 'timmy';                     //SMTP username
             $mail->Password = '3BuoPeccLXnrlgb';                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
             $mail->Port = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
             //Recipients
-            $mail->setFrom('cookcook.connect@timmygamer.nl', 'CookCook Connect');
-            $mail->addAddress('tim.vanderkloet@gmail.com', 'Tim van der Kloet');     //Add a recipient
+            $mail->setFrom('info@timmygamer.nl', 'CookCook Connect');
+            $mail->addAddress($email, ucfirst($firstname) . $lastname);     //Add a recipient
 
             //Content
             $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = 'Here is the subject';
-            $mail->Body = 'This is the HTML message body <b>in bold!</b>';
-            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            $mail->Subject = 'Email verificatie';
+            $mail->Body = 'Press this link to verify your email: <a href="http://recipesharingplatform/verify?token=' . $token . '">Verify</a';
+            $mail->AltBody = 'Press this link to verify your email: http://recipesharingplatform/verify?token=' . $token . ' ';
 
             $mail->send();
             echo 'Message has been sent';
