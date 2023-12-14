2fc60e86dc96e842d27bc1a2096c0ed2277571ba
Programmer-Timmy <tim.vanderkloet@gmail.com>
2023-12-14 11:00:11 +0100

    removed email login
    Signed-off-by: Programmer-Timmy <tim.vanderkloet@gmail.com>

--- private/controllers/mail.php
+++ private/controllers/mail.php
@@ -65,10 +65,10 @@ class mail
             //Server settings
 //            $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
             $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host = 'mail.timmygamer.nl';                     //Set the SMTP server to send through
+            $mail->Host = '';                     //Set the SMTP server to send through
             $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-            $mail->Username = 'timmy';                     //SMTP username
-            $mail->Password = '3BuoPeccLXnrlgb';                               //SMTP password
+            $mail->Username = '';                     //SMTP username
+            $mail->Password = '';                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            //Enable implicit TLS encryption
             $mail->Port = 587;
             //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
